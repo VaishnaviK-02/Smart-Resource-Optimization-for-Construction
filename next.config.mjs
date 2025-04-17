5af027a374b2f3c55642e5a20a64e65eccbe44e6
vaish <vaishnaviak2611@gmail.com>
2025-04-17 14:09:20 +0530

    Revert GitHub Pages changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,3 @@
-let userConfig = undefined
-try {
-  // try to import ESM first
-  userConfig = await import('./v0-user-next.config.mjs')
-} catch (e) {
-  try {
-    // fallback to CJS import
-    userConfig = await import("./v0-user-next.config");
-  } catch (innerError) {
-    // ignore error
-  }
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -26,29 +13,6 @@ const nextConfig = {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
-  },
-  output: 'export',
-  basePath: '/Smart-Resource-Optimization-for-Construction',
-  assetPrefix: '/Smart-Resource-Optimization-for-Construction/',
-  trailingSlash: true,
-}
-
-if (userConfig) {
-  // ESM imports will have a "default" property
-  const config = userConfig.default || userConfig
-
-  for (const key in config) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...config[key],
-      }
-    } else {
-      nextConfig[key] = config[key]
-    }
   }
 }
 
